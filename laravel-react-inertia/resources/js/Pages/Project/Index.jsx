7ad549261a1855cfe0e85ab9466eb063145721bd
ZinedineZethro <zethrozinedine@gmail.com>
2024-06-21 10:00:49 +0700

    Added Pagination and added Stylings to Status.

--- laravel-react-inertia/resources/js/Pages/Project/Index.jsx
+++ laravel-react-inertia/resources/js/Pages/Project/Index.jsx
@@ -1,4 +1,6 @@
+import Pagination from "@/Components/Pagination";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from "@/constants.jsx";
 import { Head, Link } from "@inertiajs/react";
 
 export default function index({ auth, projects }) {
@@ -28,23 +30,32 @@ export default function index({ auth, projects }) {
                 </thead>
                 <tbody>
                   {projects.data.map(project => (
-                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 text-nowrap">
+                    <tr
+                      key={project.id}
+                      className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 text-nowrap">
                       <td className="px-3 py-2">{project.id}</td>
                       <td className="px-3 py-2">
-                        <img src={project.image_path} style={{width: 60}} />
+                        <img src={project.image_path} style={{ width: 60 }} />
                       </td>
                       <td className="px-3 py-2">{project.name}</td>
-                      <td className="px-3 py-2">{project.status}</td>
+                      <td className="px-3 py-2">
+                        <span className={
+                          "px-2 py-1 rounded text-white " +
+                          PROJECT_STATUS_CLASS_MAP[project.status]
+                        }>
+                          {PROJECT_STATUS_TEXT_MAP[project.status]}
+                        </span>
+                      </td>
                       <td className="px-3 py-2">{project.created_at}</td>
                       <td className="px-3 py-2">{project.due_date}</td>
                       <td className="px-3 py-2">{project.createdBy.name}</td>
                       <td className="px-3 py-2">
                         <Link href={route('project.edit', project.id)}
-                        className="font-medium text-blue-600 dark:text-blue-500 hover:underline mx-1">
+                          className="font-medium text-blue-600 dark:text-blue-500 hover:underline mx-1">
                           Edit
                         </Link>
                         <Link href={route('project.destroy', project.id)}
-                        className="font-medium text-red-600 dark:text-red-500 hover:underline mx-1">
+                          className="font-medium text-red-600 dark:text-red-500 hover:underline mx-1">
                           Delete
                         </Link>
                       </td>
@@ -52,7 +63,7 @@ export default function index({ auth, projects }) {
                   ))}
                 </tbody>
               </table>
-
+              <Pagination links={projects.meta.links} />
             </div>
           </div>
         </div>
